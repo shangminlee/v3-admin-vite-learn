05bdfc04675f7cb5e16d8c65d4220f5b08233291
shangminlee <shangmlee@foxmail.com>
2023-04-26 17:51:22 +0800

    添加vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest" />
 
+
 import path, { resolve } from "path";
 import { type ConfigEnv, type UserConfigExport, loadEnv } from "vite";
 import vue from '@vitejs/plugin-vue';
@@ -7,6 +8,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 import svgLoader from 'vite-svg-loader';
 import UnoCSS from 'unocss/vite';
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons"
+// @ts-expect-error
 import DefineOptions from "unplugin-vue-define-options/vite"
 
 /** 配置项文档：https://cn.vitejs.dev/config */
